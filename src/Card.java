2f9328745e93f6e235def0041c37a0b5f12fd3ff
Chris Cushway <chriscushway@gmail.com>
2019-07-26 21:09:27 +0200

    Added working make file, Game class (main), Person class, Card class, Lesson class. Card class completed - no tests made yet

---
+++ src/Card.java
@@ -0,0 +1,43 @@
+public class Card{
+
+	private char suite;
+	private char value;
+	private int pointValue;
+
+	Card(char suite, char value){
+		this.suite = suite;
+		this.value = value;
+		setPointValue(value);
+	}
+
+	public char getSuite(){
+		return suite;	
+	}
+
+	public char getValue(){
+		return value;
+	}
+
+	public void setPointValue(int value){
+		//Possible better way to do this other than an if tree ? 
+		if(value != 'A' && value != 'K' && value != 'Q' && value != 'J'){
+			pointValue = 0;		
+		}else if(value == 'A'){
+			pointValue = 4;
+		}else if(value == 'K'){
+			pointValue = 3;		
+		}else if(value == 'Q'){
+			pointValue = 2;
+		}else if(value == 'J'){
+			pointValue = 1;
+		}
+	}
+
+	public int getPointValue(){
+		return pointValue;	
+	}
+
+	public String toString(){
+		return suite + "" +value;
+	}
+}
---
+++ src/Person.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Person{
+
+	private ArrayList<Card> hand;
+	private int points;
+	private String name;
+
+	Person(){
+		hand = new ArrayList<Card>();
+		points = 0;
+		name = "";
+	} 
+
+}
