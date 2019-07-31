cf5c43d17823102662f078391b81c60b7f1c20c4
Chris Cushway <chriscushway@gmail.com>
2019-07-31 21:32:55 +0200

    Removed switch statement from input capture. Added getters and setters for Person class as well as a point increment method

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
 public class Game{
 	public static void main(String args[]){
-		Card card1 = new Card('S','9');
-		System.out.println(card1);
+
+		//Create game setup
+		Person[] players = {new Person("West"),new Person("North"),new Person("East"),new Person("South")};
+		ArrayList<Card> cardArray = new ArrayList<Card>(13);
+		
+		Scanner userinput = new Scanner(System.in);
+		System.out.println("Enter level: ");
+		String level = userinput.nextLine();
+		String lesson_file = "input/input"+level+".txt";
+		try{
+			Scanner lesson = new Scanner(new File(lesson_file));
+			System.out.println("The bidding is: ");
+			String bidding = lesson.nextLine();
+			System.out.println(bidding);
+			int number_of_hands=3;
+
+			while (number_of_hands>=0){
+				String hand = lesson.nextLine();
+				String[] hand_split = hand.split(",");
+				for (String specific_suit_cards  : hand_split){
+					int length = specific_suit_cards.length();
+					for(int i=1; i<length;i++){
+						Card card = new Card(specific_suit_cards.charAt(0),specific_suit_cards.charAt(i));
+						cardArray.add(card);
+					}
+				}
+				players[number_of_hands].setHand(cardArray);
+				System.out.println(players[number_of_hands].getPersonHand());
+				number_of_hands--;
+				cardArray.clear();
+				
+
+			}
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}		
+	
 	}
 }
--- src/Person.java
+++ src/Person.java
@@ -7,14 +7,62 @@ public class Person{
 	private String name;
 
 	Person(){
-		hand = new ArrayList<Card>();
-		points = 0;
-		name = "";
+		
+		this.hand = new ArrayList<Card>();
+		this.points = 0;
+		this.name = "";
 	}
+
+	Person(String name){
+
+		this.hand = new ArrayList<Card>();
+		this.name=name;
+		this.points = 0;
+
+	}	
+
 	Person(ArrayList<Card> hand,String name){
 		this.hand = new ArrayList<Card>(hand);
 		this.points = 0;
 		this.name = name;
 	}
+
+	public void setName(String name){
+	
+		this.name=name;
+	
+	}
+
+	public void setHand(ArrayList<Card> hand){
+
+		this.hand = new ArrayList<Card>(hand);
+
+	}
+
+	public String getPersonName(){
+
+		return name;	
+
+	}
+
+	public ArrayList<Card> getPersonHand(){
+
+		return hand;
+
+	}
+
+	public int getPersonPoints(){
+
+		return points;	
+
+	}
+
+	public void incrementPoints(){
+		
+		points++;		
+
+	}
+
+
 
 }
