95948d2aa4da37ebc31765329bcfadb528920b44
Gerald Joshua <geraldjoshua7@gmail.com>
2019-08-25 18:46:10 +0200

    pushing_for_later

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Lesson"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"Trick"}]
