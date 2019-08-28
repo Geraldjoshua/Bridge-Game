2bdf56cbb4b6b4eb5e0be532fa17b31b717ae1c1
Chris Cushway <chriscushway@gmail.com>
2019-08-28 13:19:23 +0200

    improved gui to use a loop to populate board with each players hand

--- src/Card.java
+++ src/Card.java
@@ -5,69 +5,69 @@ import javax.imageio.ImageIO;
 
 public class Card{
 
-	private char suit;
-	private char value;
-	private int pointValue;
-	private BufferedImage cardImage;
+    private char suit;
+    private char value;
+    private int pointValue;
+    private BufferedImage cardImage;
 
-	Card(char suit, char value) throws IOException { 
-		this.suit = suit;
-		this.value = value;
-		this.cardImage = ImageIO.read(new File("cardImages/AS.png"));
-		setPointValue(value);
-	}
+    Card(char suit, char value) throws IOException {
+        this.suit = suit;
+        this.value = value;
+        this.cardImage = ImageIO.read(new File("cardImages/"+value+""+suit+".png"));
+        setPointValue(value);
+    }
 
-	public char getSuit(){
-		return suit;	
-	}
+    public char getSuit(){
+        return suit;
+    }
 
-	public char getValue(){
-		return value;
-	}
+    public char getValue(){
+        return value;
+    }
 
-	public void setPointValue(char value){
-		//Possible better way to do this other than an if tree ? 
-		if(value == 'A'){
-			pointValue = 4;
-		}
-		else if(value == 'K'){
-			pointValue = 3;		
-		}
-		else if(value == 'Q'){
-			pointValue = 2;
-		}
-		else if(value == 'J'){
-			pointValue = 1;
-		}
-		else{
-			pointValue=0;
-		}
-	}
+    public void setPointValue(char value){
+        //Possible better way to do this other than an if tree ?
+        if(value == 'A'){
+            pointValue = 4;
+        }
+        else if(value == 'K'){
+            pointValue = 3;
+        }
+        else if(value == 'Q'){
+            pointValue = 2;
+        }
+        else if(value == 'J'){
+            pointValue = 1;
+        }
+        else{
+            pointValue=0;
+        }
+    }
 
-	public int getPointValue(){
-		return pointValue;	
-	}
+    public int getPointValue(){
+        return pointValue;
+    }
 
-	public BufferedImage getCardImage(){
-		return cardImage;
-	}
+    public BufferedImage getCardImage(){
+        return cardImage;
+    }
 
-	public String toString(){
-		return value + "" +suit;
-	}
+    public String toString(){
+        return value + "" +suit;
+    }
 
-	//To string method to print cards with suit chracters
-	public String toString(boolean check){
-		
-		if(suit == 'S'){
-			return '\u2660'+" "+value;
-		}else if(suit == 'D'){
-			return '\u2666'+" "+value;
-		}else if(suit == 'C'){
-			return '\u2663'+" "+value;
-		}else {
-			return '\u2665'+ " "+value;
-		}		
+    //To string method to print cards with suit chracters
+    public String toString(boolean check){
 
-	}
+        if(suit == 'S'){
+            return '\u2660'+" "+value;
+        }else if(suit == 'D'){
+            return '\u2666'+" "+value;
+        }else if(suit == 'C'){
+            return '\u2663'+" "+value;
+        }else {
+            return '\u2665'+ " "+value;
+        }
+
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -11,285 +11,220 @@ import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 public class Game{
-	public static void main(String args[]) throws IOException, InterruptedException{
+    public static void main(String args[]) throws IOException, InterruptedException{
 
-		Scanner userinput = new Scanner(System.in);
-		System.out.println("Enter level: ");
-		String level = userinput.nextLine();
-		String lesson_file = "input/input"+level+".txt";
-		Lesson lesson = new Lesson(lesson_file);
-		
-		ArrayList<String> copyBestCase = lesson.getBestCase();
-		ArrayList<String> cardsPlayed = new ArrayList<String>();
-		
-		System.out.println("------------------------------------------------------------------\n");
-		
-		int playerTurn = 0;
+        Scanner userinput = new Scanner(System.in);
+        System.out.println("Enter level: ");
+        String level = userinput.nextLine();
+        String lesson_file = "input/input"+level+".txt";
+        Lesson lesson = new Lesson(lesson_file);
 
-		//Setting up gui for game
+        ArrayList<String> copyBestCase = lesson.getBestCase();
+        ArrayList<String> cardsPlayed = new ArrayList<String>();
 
-		JFrame window = new JFrame("Bridge Tutor");
-		
- 
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println("------------------------------------------------------------------\n");
+
+        int playerTurn = 0;
+
+        //Setting up gui for game
+
+        JFrame window = new JFrame("Bridge Tutor");
+
+        window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		/*A way to get the screens size will be used later
 		/*For UI components
 		*/
-		Toolkit tk = Toolkit.getDefaultToolkit();  
-		int xSize = ((int) tk.getScreenSize().getWidth());  
-		int ySize = ((int) tk.getScreenSize().getHeight()); 
-		window.setSize(xSize,ySize);
-		
-		//setting up panel - it is like the "glass" in the window
-		JPanel contentPane = new JPanel();
-		contentPane.setLayout(null);
-		
-		//Array of Jlabels to act as gui hand
-		/*This is just code testing to
-			see how guis work
-			*/
-		ArrayList<JLabel> cardLabels = new ArrayList<JLabel>();
-		for(int i=0;i<13;i++){
-			JLabel cardLabel = new JLabel(new ImageIcon(lesson.getPlayers().get(playerTurn).getCard(i).getCardImage()));
-			cardLabel.setSize(80,120);
-			cardLabel.addMouseListener(new MouseAdapter() {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        int xSize = ((int) tk.getScreenSize().getWidth());
+        int ySize = ((int) tk.getScreenSize().getHeight());
+        //window.setSize(xSize,ySize);
 
-			    @Override
-			    public void mouseEntered(MouseEvent evt) {
-				Point pt = cardLabel.getLocation();
-				int x = pt.x;
-				int y = pt.y;
-				cardLabel.setLocation(x,y-20);
-			    }
 
-			    @Override
-			    public void mouseExited(MouseEvent evt) {
-				Point pt = cardLabel.getLocation();
-				int x = pt.x;
-				int y = pt.y;
-				cardLabel.setLocation(x,y+20);
-			    }
-			
-			});
-			cardLabels.add(cardLabel);
-		}
-		contentPane.setPreferredSize(new Dimension(xSize, 160));
-		final int xDim = 40;
-		System.out.println(contentPane.getPreferredSize().getHeight());
-		int marginX=xSize/2 - (40*13)/2 - 40;
-		for(JLabel j:cardLabels){
-    			contentPane.add(j);
-			
-			j.setLocation(marginX,30);
-			marginX+=xDim;
-		}
-		
+        ArrayList<JLabel> cardLabels = new ArrayList<JLabel>();
+        JPanel[] panels = new JPanel[4];
+        for(int i=0;i<panels.length;i++){
+            panels[i] = new JPanel();
+            panels[i].setOpaque(false);
+            panels[i].setLayout(null);
+            if(i%2 == 0) {
 
-		
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout(null);
-		topPanel.setPreferredSize(new Dimension(xSize, 160));
-		topPanel.setBackground(Color.red);
-		
-		playerTurn++;
-		ArrayList<JLabel> cardLabels2 = new ArrayList<JLabel>();
-		for(int i=0;i<13;i++){
-			JLabel cardLabel = new JLabel(new ImageIcon(lesson.getPlayers().get(playerTurn).getCard(i).getCardImage()));
-			cardLabel.setSize(80,120);
-			cardLabel.addMouseListener(new MouseAdapter() {
+                panels[i].setPreferredSize(new Dimension(xSize,200));
+            } else{
+                panels[i].setPreferredSize(new Dimension(400, ySize));
+            }
 
-			    @Override
-			    public void mouseEntered(MouseEvent evt) {
-				Point pt = cardLabel.getLocation();
-				int x = pt.x;
-				int y = pt.y;
-				cardLabel.setLocation(x,y-20);
-			    }
+        }
 
-			    @Override
-			    public void mouseExited(MouseEvent evt) {
-				Point pt = cardLabel.getLocation();
-				int x = pt.x;
-				int y = pt.y;
-				cardLabel.setLocation(x,y+20);
-			    }
-			
-			});
-			cardLabels2.add(cardLabel);
-		}
-		marginX=xSize/2 - (40*13)/2 - 40;
-		for(JLabel j:cardLabels2){
-    			topPanel.add(j);
-			
-			j.setLocation(marginX,30);
-			marginX+=xDim;
-		}
-		
-		
-		JPanel leftPanel = new JPanel();
-		leftPanel.setPreferredSize(new Dimension(400, ySize));
-		leftPanel.setBackground(Color.blue);
+        panels[0].setBackground(Color.RED);
+        panels[1].setBackground(Color.BLACK);
+        panels[2].setBackground(Color.YELLOW);
+        panels[3].setBackground(Color.BLUE);
+        for(int i=0;i<panels.length;i++){
+            for(int j=0;j<13;j++){
+                final JLabel cardLabel = new JLabel(new ImageIcon(lesson.getPlayers().get(i).getCard(j).getCardImage()));
+                cardLabel.setSize(120,140);
+                cardLabel.addMouseListener(new MouseAdapter() {
 
-		JPanel centerPanel = new JPanel();
-		centerPanel.setPreferredSize(new Dimension(600, 600));
-        	centerPanel.setMinimumSize(new Dimension(600, 600));
-		centerPanel.setBackground(Color.green);
+                    @Override
+                    public void mouseEntered(MouseEvent evt) {
+                        Point pt = cardLabel.getLocation();
+                        int x = pt.x;
+                        int y = pt.y;
+                        cardLabel.setLocation(x,y-20);
+                    }
 
-		JPanel rightPanel = new JPanel();
-		rightPanel.setLayout(null);
-		rightPanel.setPreferredSize(new Dimension(400, ySize));
-		rightPanel.setBackground(Color.orange);
-		playerTurn++;
-		ArrayList<JLabel> cardLabels3 = new ArrayList<JLabel>();
-		for(int i=0;i<13;i++){
-			JLabel cardLabel = new JLabel(new ImageIcon(lesson.getPlayers().get(playerTurn).getCard(i).getCardImage()));
-			cardLabel.setSize(80,120);
-			cardLabel.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseExited(MouseEvent evt) {
+                        Point pt = cardLabel.getLocation();
+                        int x = pt.x;
+                        int y = pt.y;
+                        cardLabel.setLocation(x,y+20);
+                    }
 
-			    @Override
-			    public void mouseEntered(MouseEvent evt) {
-				Point pt = cardLabel.getLocation();
-				int x = pt.x;
-				int y = pt.y;
-				cardLabel.setLocation(x-20,y);
-			    }
+                });
+                cardLabels.add(cardLabel);
+            }
+            double panelWidth = panels[i].getPreferredSize().getWidth();
+            System.out.println(panelWidth);
+            int xDim;
+            int startMargin=60;
+            int marginX;
+            if(i%2 == 0){
+                startMargin=(int)panelWidth/2 - (50*13)/2 - 50;
+                xDim = 50;
+            }else{
+                xDim=40;
+            }
+            marginX=startMargin;
+            System.out.println(marginX);
+            int marginY = 30;
+            for(JLabel j:cardLabels){
+                panels[i].add(j);
+                j.setLocation(marginX,marginY);
+                marginX+=xDim;
+                if(marginX+120>=panelWidth){
 
-			    @Override
-			    public void mouseExited(MouseEvent evt) {
-				Point pt = cardLabel.getLocation();
-				int x = pt.x;
-				int y = pt.y;
-				cardLabel.setLocation(x+20,y);
-			    }
+                    marginX=startMargin;
+                    marginY+=130;
 
-				@Override
-				public void mouseClicked(MouseEvent evt){
+                }
+            }
+            cardLabels.clear();
+        }
 
-					
-					
-					Container parent = cardLabel.getParent();
-					parent.remove(cardLabel);
-					parent.validate();
-					parent.repaint();
-					centerPanel.add(cardLabel);
-					cardLabel.setLocation(0,0);
 
-				}
-			
-			});
-			cardLabels3.add(cardLabel);
-		}
-				
-		final int yDim = 30;
-		int marginY=0;
-		for(JLabel j:cardLabels3){
-    			rightPanel.add(j);
-			marginY+=10;
-			j.setLocation(20,marginY);
-			marginY+=yDim;
-		}
-		
-		
-	   	window.add(topPanel, BorderLayout.NORTH);
-        	window.add(leftPanel, BorderLayout.WEST);
-        	window.add(centerPanel, BorderLayout.CENTER);
-        	window.add(rightPanel, BorderLayout.EAST);
-        	window.add(contentPane, BorderLayout.SOUTH);
+        JPanel centerPanel = new JPanel();
 
-		window.pack();
-        	window.setLocationRelativeTo(null);
-		window.setVisible(true);
+        centerPanel.setLayout(null);
+        centerPanel.setPreferredSize(new Dimension(600,600));
+        centerPanel.setMinimumSize(new Dimension(600, 600));
+        centerPanel.setOpaque(false);
 
-		//-----FIRST PLAY OF THE GAME----------------------------------------------------------------------//
-		System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " is now playing.");
-		System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName()+" played: "+lesson.getFirstCardPlayed());
-		System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " completed their turn."+"\n");
-		copyBestCase.remove(0);
-		lesson.isValid(lesson.getFirstCardPlayed(),lesson.getPlayers().get(playerTurn));
-		cardsPlayed.add(lesson.getFirstCardPlayed());
-		playerTurn++;
-		//-----FIRST PLAY OF THE GAME END----------------------------------------------------------------------//		
-	
+        window.add(panels[2], BorderLayout.NORTH);
+        window.add(panels[3], BorderLayout.WEST);
+        window.add(centerPanel, BorderLayout.CENTER);
+        window.add(panels[1], BorderLayout.EAST);
+        window.add(panels[0], BorderLayout.SOUTH);
+        
+        window.pack();
+        window.setLocationRelativeTo(null);
 
-		//---------------------------------------------------------THE ACTUAL GAME-----------------------------------------------------///
+        window.getContentPane().setBackground(new Color(0, 134, 64));
 
-		//Variables to be tracked
-		int tricks = 0;			//tricks in the game (max is 13)	
-		boolean trickWinner = false;
-		boolean claim = false;		//If a person plays claim
-		
-		while(tricks < 13){ //max tricks is 13 unless claim played beforehand
-				if(tricks==0){
-					copyBestCase = lesson.getBestCase1(tricks);
-					copyBestCase.remove(0);
-				}
-				else{
-					copyBestCase = lesson.getBestCase1(tricks);
-				}
-				while(playerTurn<4){
-					System.out.print("Cards played: ");
-					for(String card:cardsPlayed){
-						System.out.print(card+" ");				
-					}
-					System.out.print("\n\n");
-					System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " is now playing.");
-					System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " Please play a card by typing the number and then the suit e.g 6D \n");
-					lesson.getPlayers().get(playerTurn).printNiceHand();
-					System.out.println("\n");
-					System.out.println("------------------------------------------------------------------");
-					
-					String card = userinput.nextLine();
-					//If not the first turn
-					if(playerTurn>0){
+        //window.repaint();
+        window.setVisible(true);
 
-						while(!lesson.isValid(card,lesson.getPlayers().get(playerTurn))){
-							System.out.println("That card is not a valid play please play another card");
-							card = userinput.nextLine();
-						}
-					//else its the first turn we need to set the suit	
-					}else{
-						lesson.setFirstCardPlayed(card);
-						while(!lesson.isValid(card,lesson.getPlayers().get(playerTurn))){
+        //-----FIRST PLAY OF THE GAME----------------------------------------------------------------------//
+        System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " is now playing.");
+        System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName()+" played: "+lesson.getFirstCardPlayed());
+        System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " completed their turn."+"\n");
+        copyBestCase.remove(0);
+        lesson.isValid(lesson.getFirstCardPlayed(),lesson.getPlayers().get(playerTurn));
+        cardsPlayed.add(lesson.getFirstCardPlayed());
+        playerTurn++;
+        //-----FIRST PLAY OF THE GAME END----------------------------------------------------------------------//
 
-							System.out.println("That card is not a valid play please play another card");
-							card = userinput.nextLine();
-						}
-				
-						
-					}
-					
-					if(!card.equals(copyBestCase.get(0))){
-						String bestPlay = lesson.getPlayers().get(playerTurn).bestCaseInHand(copyBestCase);
-						System.out.println("The card that should have been played was: "+bestPlay);
-						
-					}
-					//Add the points of the play to the player i.e playing an ACE adds 14 points to player
-					lesson.getPlayers().get(playerTurn).addPoints(lesson.getPlayPoints(card));
-					cardsPlayed.add(card);
 
-					//Only let the person claim if it is in fact a stage in the game where claiming will actually win them the game
-					if(card.equals("CLAIM") && copyBestCase.equals("CLAIM")){
-						claim=true;
-						break;
-					}
-					copyBestCase.remove(0);
-					playerTurn++;
-				}
+        //---------------------------------------------------------THE ACTUAL GAME-----------------------------------------------------///
 
-			//If they claimed
-			if(claim){
-				lesson.getPlayers().get(playerTurn).setTrickWins(13 - tricks + lesson.getPlayers().get(playerTurn).getTrickWins());
-			}
-			lesson.decideWinner();
-			lesson.reorderPlayers();
-			cardsPlayed.clear();
-			playerTurn=0;
-			tricks++;
-			
-		}
+        //Variables to be tracked
+        int tricks = 0;			//tricks in the game (max is 13)
+        boolean trickWinner = false;
+        boolean claim = false;		//If a person plays claim
 
-		lesson.decideGameWinner();
-						
-	}
+        while(tricks < 13){ //max tricks is 13 unless claim played beforehand
+            if(tricks==0){
+                copyBestCase = lesson.getBestCase1(tricks);
+                copyBestCase.remove(0);
+            }
+            else{
+                copyBestCase = lesson.getBestCase1(tricks);
+            }
+            while(playerTurn<4){
+                System.out.print("Cards played: ");
+                for(String card:cardsPlayed){
+                    System.out.print(card+" ");
+                }
+                System.out.print("\n\n");
+                System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " is now playing.");
+                System.out.println(lesson.getPlayers().get(playerTurn).getPlayerName() + " Please play a card by typing the number and then the suit e.g 6D \n");
+                lesson.getPlayers().get(playerTurn).printNiceHand();
+                System.out.println("\n");
+                System.out.println("------------------------------------------------------------------");
+
+                String card = userinput.nextLine();
+                //If not the first turn
+                if(playerTurn>0){
+
+                    while(!lesson.isValid(card,lesson.getPlayers().get(playerTurn))){
+                        System.out.println("That card is not a valid play please play another card");
+                        card = userinput.nextLine();
+                    }
+                    //else its the first turn we need to set the suit
+                }else{
+                    lesson.setFirstCardPlayed(card);
+                    while(!lesson.isValid(card,lesson.getPlayers().get(playerTurn))){
+
+                        System.out.println("That card is not a valid play please play another card");
+                        card = userinput.nextLine();
+                    }
+
+
+                }
+
+                if(!card.equals(copyBestCase.get(0))){
+                    String bestPlay = lesson.getPlayers().get(playerTurn).bestCaseInHand(copyBestCase);
+                    System.out.println("The card that should have been played was: "+bestPlay);
+
+                }
+                //Add the points of the play to the player i.e playing an ACE adds 14 points to player
+                lesson.getPlayers().get(playerTurn).addPoints(lesson.getPlayPoints(card));
+                cardsPlayed.add(card);
+
+                //Only let the person claim if it is in fact a stage in the game where claiming will actually win them the game
+                if(card.equals("CLAIM") && copyBestCase.equals("CLAIM")){
+                    claim=true;
+                    break;
+                }
+                copyBestCase.remove(0);
+                playerTurn++;
+            }
+
+            //If they claimed
+            if(claim){
+                lesson.getPlayers().get(playerTurn).setTrickWins(13 - tricks + lesson.getPlayers().get(playerTurn).getTrickWins());
+            }
+            lesson.decideWinner();
+            lesson.reorderPlayers();
+            cardsPlayed.clear();
+            playerTurn=0;
+            tricks++;
+
+        }
+
+        lesson.decideGameWinner();
+
+    }
 }
