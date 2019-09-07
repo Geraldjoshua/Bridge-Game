9dffcff7871fb19700edeb71e865017969be50bf
Chris Cushway <chriscushway@gmail.com>
2019-09-07 18:24:52 +0200

    added select lesson button

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -10,8 +10,10 @@ import java.awt.event.*;
 public class MenuScreen extends JFrame{
 	
 	private final int xSize,ySize;
+	private final Dimension smallButton = new Dimension(20,20),largeButton = new Dimension(200,50);
+	private int headingHeight;
 	private BackgroundPanel bgPanel;
-	private JButton exitButton;
+	private JButton exitButton,selectLesson;
 
 	MenuScreen(int xSize, int ySize)throws IOException{
 		this.xSize = xSize;
@@ -29,6 +31,8 @@ public class MenuScreen extends JFrame{
 	public void addComponentsToPanel(){
 		
 		initExitButton();
+		initHeader();
+		initSelectLessonButton();
 		
 	}
 
@@ -47,6 +51,29 @@ public class MenuScreen extends JFrame{
         exitButton.setLocation(this.getWidth()-exitButton.getWidth()-20,20);
 		exitButton.setFocusPainted(false);
 		bgPanel.add(exitButton);
+	}
+
+	public void initHeader(){
+		JLabel header = new JLabel("<html><h1 style='color:white;font-weight:bold;'>Bridge Tutor v 1.0</h1></html>");
+		header.setSize(header.getPreferredSize());
+		header.setLocation(this.getWidth()/2 - header.getWidth()/2,20);
+		headingHeight = header.getHeight();
+		bgPanel.add(header);
+	}
+
+	public JButton initButton(JButton b,String text,Color bg,Color tc,Dimension d,Point p,boolean visible){
+		b = new JButton(text);
+		b.setSize(d);
+		b.setLocation(p.x - b.getWidth()/2,p.y);
+		b.setBackground(bg);
+		b.setForeground(tc);
+		b.setVisible(visible);
+		return b;
+	}
+
+	public void initSelectLessonButton(){
+		selectLesson=initButton(selectLesson,"<html><h3 style='color:red;'>SELECT LESSON</h3></html>",Color.white,Color.red,largeButton,new Point(this.getWidth()/2,40 + headingHeight),true);
+		bgPanel.add(selectLesson);
 	}
 
 }
